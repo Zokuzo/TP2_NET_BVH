17c0f94fca76c8ff91590da9338af36bd8e8aa6a
VirgileLecomte <virgile.lecomte@etu.estia.fr>
2025-02-28 14:10:22 +0100

    Static Install Uninstall Play Stop

---
+++ Gauniv.Client/Model/GlobalStorage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gauniv.Client.Model
+{
+    class GlobalStorage
+    {
+        public static Dictionary<String, Process> ProcessIds { get; set; } = new ();
+    }
+}
